cfeaa447b358a756c3447e97025342aa819dea87
Soton <77579218+soton371@users.noreply.github.com>
2021-02-26 00:40:58 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+
+
+def f(x):
+    return 1/(1+x)**2 #you can change this function for your equation
+
+loLimit = float(input("Input the lower limit:"))
+upLimit = float(input("Input the upper limit:"))
+subInterval = float(input("Input the number of sub-intervals:"))
+sum2 = 0.0
+
+h = (upLimit-loLimit)/subInterval
+
+sum1 = f(loLimit)+f(upLimit)
+
+for i in range(1, int(subInterval)):
+    sum2 += f(loLimit + i*h)
+    
+fx = (h/2)*(sum1 + 2*sum2)
+
+print("Your result is:{:.5f}".format(fx))
+
+
+
+
+
